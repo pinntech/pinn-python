3d05a1aec5871b8e32e82ae1add7d93f7924c8b8
Dave Westerhoff <david@pinn.ai>
2019-03-29 12:10:39 -0400

    cleaning up error codes

--- pinn/errors.py
+++ pinn/errors.py
@@ -63,15 +63,11 @@ class APIConnectionError(PinnError):
     pass
 
 
-class SignatureVerificationError(PinnError):
-    pass
-
-
 PINN_ERROR_CODE_MAP = {
-    requests.codes.internal_server_error: 'internal_server_error',
-    requests.codes.bad_gateway: 'bad_gateway',
-    requests.codes.service_unavailable: 'service_unavailable',
-    requests.codes.gateway_timeout: 'gateway_timeout'
+    requests.codes.internal_server_error: 'internal_server_error',  # 500
+    requests.codes.service_unavailable: 'service_unavailable',      # 502
+    requests.codes.gateway_timeout: 'gateway_timeout',              # 503
+    requests.codes.bad_gateway: 'bad_gateway'                       # 504
 }
 
 PINN_ERROR_TYPE_MAP = {
@@ -83,15 +79,30 @@ PINN_ERROR_TYPE_MAP = {
     'conflict': RequestFailedError,              # 409
     'unprocessable_entity': RequestFailedError,  # 422
     'rate_limit_exceeded': RateLimitError,       # 429
-    'internal_server_error': APIError            # 500
+    'internal_server_error': APIError,           # 500
+    'service_unavailable': APIConnectionError,   # 502
+    'bad_gateway': APIConnectionError,           # 503
+    'gateway_timeout': APIConnectionError        # 504
 }
 
 
 # Non API Errors
 # --------------
 class IDTokenVerificationError(Exception):
+    """This exception can be raised during an attempt to verify an ID token.
+
+    Potentially this can be from:
+        - An invalid JWT signature
+        - Missing required claims
+        - `amr` claim mismatch from expectations
+    """
     pass
 
 
 class ConfigurationError(Exception):
+    """This error is thrown fast if the library is misconfigured.
+
+    For example, if you attempt to call the library without a `secret_key` value
+    set a ConfigurationError will be thrown.
+    """
     pass
