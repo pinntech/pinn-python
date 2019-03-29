296f50d9eb5f58d397a6125b4c83728b89fe6a01
Dave Westerhoff <david@pinn.ai>
2019-03-29 13:40:59 -0400

    Documenting errors/exceptions

--- pinn/errors.py
+++ pinn/errors.py
@@ -10,6 +10,7 @@ class PinnError(Exception):
     """Base exception class for a Pinn API Error."""
 
     def __init__(self, response):
+        """Create a PinnError object with a response dictionary object."""
         try:
             error = response.json()['error']
             self._message = error["message"]
@@ -23,7 +24,7 @@ class PinnError(Exception):
 
     @staticmethod
     def from_response(response):
-        """Create an error of the right class from an API response."""
+        """Create an error of the right PinnError subclass from an API response."""
         if response.status_code == requests.codes.internal_server_error:
             return APIError(response)
         if response.status_code in [requests.codes.bad_gateway,
@@ -36,26 +37,52 @@ class PinnError(Exception):
 
 
 class InvalidRequestError(PinnError):
+    """The request was malformed and could not be understood by the server.
+
+    This could mean that an incorrect type was sent for a parameter, an invalid HTTP
+    method was used or that the resource you are requesting does not exist. Do not
+    attempt to retry the request.
+    """
     pass
 
 
 class AuthenticationError(PinnError):
+    """The secret key set is invalid and cannot be used to authenticate your request."""
     pass
 
 
 class RequestFailedError(PinnError):
+    """The request was properly formed with valid syntax, but could not be performed.
+
+    This occurs in scenarios where circumstances prevent an operation from being executed,
+    typically this can be situations like invalid resource state to perform the op.
+    """
     pass
 
 
-class PermissionError(PinnError):
+class ForbiddenError(PinnError):
+    """This error is returned if you attempt to access a resource you don't own.
+
+    Do not attempt to retry this error, and if you receieve this error it likely
+    means you have an incorrect `secret_key` set on the library.
+    """
     pass
 
 
 class RateLimitError(PinnError):
+    """The API rate limit has been exceeded.
+
+    You should attempt to retry this request after some time has passed.
+    """
     pass
 
 
 class APIError(PinnError):
+    """An internal server error caused the request to fail.
+
+    Pinn is automatically notified if issues are encountered that are internal to our
+    service. You should retry this request in case the error was intermittent.
+    """
     pass
 
 
@@ -74,7 +101,7 @@ PINN_ERROR_CODE_MAP = {
 PINN_ERROR_TYPE_MAP = {
     'bad_request': InvalidRequestError,          # 400
     'unauthorized': AuthenticationError,         # 401
-    'forbidden': AuthenticationError,            # 403
+    'forbidden': ForbiddenError,                 # 403
     'not_found': InvalidRequestError,            # 404
     'method_not_allowed': InvalidRequestError,   # 405
     'conflict': RequestFailedError,              # 409
